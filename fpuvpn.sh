3ae364b31bbe3b0163d858a3d433586b05b8d23c
James Andrews <jandrews7348@floridapoly.edu>
2019-09-12 19:42:04 -0400

    Update SU

--- fpuvpn.sh
+++ fpuvpn.sh
@@ -4,4 +4,4 @@
 #James Andrews <jandrews7348@floridapoly.edu>
 
 echo Starting up docker...
-docker run -ti --rm --privileged --net=host openconnect || echo Failed to run! Please install docker-ce!
+sudo docker run -ti --rm --privileged --net=host openconnect || echo Failed to run! Please install docker-ce!
