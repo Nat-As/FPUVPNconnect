0f63f1cfb8e980a99bc3403a8244afecf3a2ab83
James Andrews <jandrews7348@floridapoly.edu>
2020-02-25 16:58:15 -0500

    require root check

--- fpuvpn.sh
+++ fpuvpn.sh
@@ -4,4 +4,5 @@
 #James Andrews <jandrews7348@floridapoly.edu>
 
 echo Starting up docker...
+sudo apt update || echo Root required!
 sudo docker run -ti --rm --privileged --net=host openconnect || echo Failed to run! Please install docker-ce!
