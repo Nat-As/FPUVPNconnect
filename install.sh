34e3a66b2c68419000154addbeb83013723c715d
James Andrews <jandrews7348@floridapoly.edu>
2020-07-13 09:35:52 -0400

    fix

--- install.sh
+++ install.sh
@@ -2,17 +2,15 @@
 
 #VPN Installation Script for FPU (Unofficial)
 #James Andrews <jandrews7348@floridapoly.edu>
-function getidgud(){
-    printf "USER:"
-    read uname
-    printf "PASSWORD:"
-    read passwrd
-}
+
 function installer(){
     sudo apt-get install \
         build-essential gettext autoconf automake libproxy-dev \
         libxml2-dev libtool vpnc-scripts pkg-config \
         libgnutls-dev
+    ./configure
+    make
+    make install
 }
 function installdeb(){
     sudo apt-get install \
@@ -21,19 +19,3 @@ function installdeb(){
         libgnutls28-dev
 }
 installer || installdeb
-function dockerinst(){
-    #getidgud
-    #docker build --build-arg username=$uname --build-arg pass=$passwrd -t openconnect .
-    #Connot build docker if daemon is not running.
-    docker build -t openconnect . || systemctl start docker && docker build -t openconnect .
-    docker run -ti --rm --privileged --net=host openconnect
-    #./openconnect --protocol=gp --csd-wrapper=hipreport.sh vpn.floridapoly.edu
-}
-git clone https://github.com/Nat-As/openconnect.git
-chmod -Rv 0777 openconnect
-cd openconnect
-./autogen.sh
-./configure
-make
-sudo make install && sudo ldconfig
-dockerinst
