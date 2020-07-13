9eb4ca70d49607c2816034176cbe2a1db61d4d23
James Andrews <jandrews7348@floridapoly.edu>
2020-07-13 10:06:12 -0400

    startup openconnect properly

--- fpuvpn.sh
+++ fpuvpn.sh
@@ -2,7 +2,10 @@
 
 #Connect to the FPU VPN after installing it with ./install.sh
 #James Andrews <jandrews7348@floridapoly.edu>
+echo "Welcome to Floridapoly.org"
+echo "--------------------------"
+echo "Please enter your username:"
+read u
 
-echo Starting up docker...
-sudo apt update || echo Root required!
-sudo docker run -ti --rm --privileged --net=host openconnect || echo Failed to run! Please install docker-ce!
+echo "Connecting..."
+sudo ./openconnect --protocol=gp -u $u vpn.floridapoly.edu
